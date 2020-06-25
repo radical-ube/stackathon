8ab15079b0a6610b471693d96dde351d85bfe9a9
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-25 15:12:20 -0400

    reset defaults and created start page

---
+++ client/components/start.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+export const Start = props => {
+  return (
+    <div>
+      <h3>Start Page</h3>
+    </div>
+  )
+}
+
+export default connect(null)(Start)
