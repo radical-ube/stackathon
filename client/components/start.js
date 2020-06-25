3101d82c69479de8a1aaad89318eab5301f2b1f0
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-25 15:42:17 -0400

    installed p5 and added a canvas component

--- client/components/start.js
+++ client/components/start.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {default as Canvas} from './canvas'
 
 export const Start = props => {
   return (
     <div>
       <h3>Start Page</h3>
+      <Canvas />
     </div>
   )
 }
