a71569324d0de9cef80da6208abe1c550f060e43
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-25 20:21:47 -0400

    uninstall p5 and refactor Scene component

--- client/components/scene.js
+++ client/components/scene.js
@@ -1,56 +1,76 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import p5 from 'p5'
 import Matter from 'matter-js'
+// import {Box} from './world/box'
 
 class Scene extends React.Component {
   constructor(props) {
     super(props)
-    this.myRef = React.createRef()
+    this.state = {}
   }
 
-  Sketch = sketch => {
+  componentDidMount() {
     // module aliases from MatterJS
-    const {Engine, World, Bodies, Mouse, MouseConstraint} = Matter
+    const {Engine, Render, World, Bodies, Mouse, MouseConstraint} = Matter
 
     // create an engine and a world
     const engine = Engine.create({})
     const world = engine.world
+
+    // create a renderer
+    const render = Render.create({
+      element: this.refs.scene,
+      engine,
+      options: {
+        width: 600,
+        height: 600,
+        wireframes: false
+      }
+    })
+
+    // create bodies
+    const ballA = Bodies.circle(210, 100, 30, {restitution: 0.5})
+    const ballB = Bodies.circle(110, 50, 30, {restitution: 0.5})
+
+    // add walls into the world
+    World.add(world, [
+      Bodies.rectangle(200, 0, 600, 50, {isStatic: true}),
+      Bodies.rectangle(200, 600, 600, 50, {isStatic: true}),
+      Bodies.rectangle(600, 300, 50, 600, {isStatic: true}),
+      Bodies.rectangle(0, 300, 50, 600, {isStatic: true})
+    ])
+
+    // add balls into the world
+    World.add(world, [ballA, ballB])
+
+    // add mouse constraint
+    const mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      })
+
+    // add the mouse constraint to the world
+    World.add(world, mouseConstraint)
+
+    Matter.Events.on(mouseConstraint, 'mousedown', function(event) {
+      World.add(engine.world, Bodies.circle(150, 50, 30, {restitution: 0.7}))
+    })
+
     // run the engine
     Engine.run(engine)
 
-    // create bodies
-    let boxA
-    let boxB
-    let ground
-
-    sketch.setup = () => {
-      sketch.createCanvas(800, 600)
-
-      boxA = Bodies.rectangle(400, 200, 80, 80)
-      boxB = Bodies.rectangle(450, 50, 80, 80)
-      ground = Bodies.rectangle(400, 510, 810, 60, {
-        isStatic: true
-      })
-
-      // add your objects into the world
-      World.add(world, [boxA, boxB, ground])
-      console.log(boxA, boxB)
-    }
-    sketch.draw = () => {
-      sketch.background(0)
-      sketch.fill(255)
-      sketch.rect(boxA.position.x, boxA.position.y, 80, 80)
-      sketch.rect(boxB.position.x, boxB.position.y, 80, 80)
-    }
-  }
-
-  componentDidMount() {
-    this.myP5 = new p5(this.Sketch, this.myRef.current)
+    // run the render
+    Render.run(render)
   }
 
   render() {
-    return <div ref={this.myRef} />
+    return <div ref="scene" />
   }
 }
 
