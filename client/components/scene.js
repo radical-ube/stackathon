5a249ebf3bb0ba657208a63ad88eaa35ac9f9eb5
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-25 22:37:00 -0400

    matterJS first scene

---
+++ client/components/scene.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import p5 from 'p5'
+import Matter from 'matter-js'
+
+class Scene extends React.Component {
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef()
+  }
+
+  Sketch = sketch => {
+    let x = 100
+    let y = 100
+    const {Engine, World, Bodies} = Matter
+
+    let engine
+    let world
+
+    // bodies
+    let boxA = Bodies.rectangle(400, 200, 80, 80)
+    let boxB = Bodies.rectangle(440, 240, 80, 80)
+    let ground = Bodies.rectangle(400, 380, 810, 60, {isStatic: true})
+
+    sketch.setup = () => {
+      sketch.createCanvas(800, 600)
+      engine = Engine.create()
+      world = engine.world
+
+      Engine.run(engine)
+      World.add(world, [boxA, boxB, ground])
+    }
+    sketch.draw = () => {
+      sketch.background(0)
+      sketch.fill(255)
+      sketch.rect(boxA.position.x, boxA.position.y, 80, 80)
+      sketch.rect(boxB.position.x, boxB.position.y, 80, 80)
+    }
+  }
+
+  componentDidMount() {
+    this.myP5 = new p5(this.Sketch, this.myRef.current)
+  }
+
+  render() {
+    return <div ref={this.myRef} />
+  }
+}
+
+export default connect(null)(Scene)
